0207dce132c9fb17efd1f6cf65cc22eae45e9ac0
Osorabrian <osorabrian@gmail.com>
2023-01-20 07:27:29 +0300

    complted assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import MoviesList from "./MoviesList";
+import { Route, useRouteMatch } from "react-router-dom";
+import MoviesList from "./MoviesList.js";
+import MovieShow from "./movieshow.js";
 
 function MoviesPage({ movies }) {
+
+  const match = useRouteMatch()
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
